9bebb979b999212c603bcd92fc1db31b9aa3d8fd
Andrew <andrewlokoffical@gmail.com>
2025-03-03 22:29:17 +0100

    Added sidebar menu with dynamic items

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,8 +1,24 @@
-export default function Dashboard({
+import Image from "next/image"
+import Link from "next/link";
+
+import Menu from "@/components/Menu"
+
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>dashboard{children}</div>
+  return <div className="h-screen flex">
+    {/* LEFT */}
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]" >
+      <Link href="/" className="flex items-center justify-center lg:justify-start gap-2 p-4">
+        <Image src="/logo.png" alt="logo" width={32} height={32} />
+        <span className="hidden lg:block">Dashboard</span>
+      </Link>
+      <Menu />
+    </div>
+    {/* RIGHT */}
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">f</div>
+  </div>
 
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Image from "next/image"
+
 const menuItems = [
   {
     title: "MENU",
@@ -12,7 +15,7 @@ const menuItems = [
         icon: "/teacher.png",
         label: "Teachers",
         href: "/list/teachers",
-        visible: ["admin", "teacher"],
+        visible: ["admin", "teacher"],    
       },
       {
         icon: "/student.png",
@@ -112,3 +115,25 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2 pl-4" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => (
+            <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
+              <Image src={item.icon} alt="" width={20} height={20}/>
+              <span>{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
