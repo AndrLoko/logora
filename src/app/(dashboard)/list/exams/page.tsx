60b730915b18539ead82fdaedda8d122944ce4bb
AndrLoko <andrewlokoffical@gmail.com>
2025-04-28 21:52:55 +0200

    Add Exams Page

--- src/app/(dashboard)/list/exams/page.tsx
+++ src/app/(dashboard)/list/exams/page.tsx
@@ -1,14 +1,16 @@
 import Pagination from '@/components/Pagination';
 import Table from '@/components/Table';
 import TableSearch from '@/components/TableSearch';
-import { role, subjectsData} from '@/lib/data';
+import {examsData, role} from '@/lib/data';
 import Image from 'next/image'
 import Link from 'next/link';
 
-type Subject = {
+type Exam = {
     id:number,
-    name: string,
-    teachers: string[],
+    subject: string,
+    class: string,
+    teacher: string,
+    date: string,
 }
 
 const columns = [
@@ -17,8 +19,17 @@ const columns = [
         accessor: 'name'
     },
     {
-        header: "Teachers", 
-        accessor: 'teachers',
+        header: "Class", 
+        accessor: 'class',
+    },
+    {
+        header: "Teacher",
+        accessor: "teacher",
+        className: 'hidden md:table-cell'
+    },
+    {
+        header: "Date",
+        accessor: "date",
         className: 'hidden md:table-cell'
     },
     {
@@ -27,11 +38,13 @@ const columns = [
     },
 ]
 
-const SubjectListPage = () => {
-    const renderRow = (item: Subject) => (
+const ExamListPage = () => {
+    const renderRow = (item: Exam) => (
         <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight'>
-            <td className='flex items-center gap-4 p-4'>{item.name}</td> 
-            <td className='hidden md:table-cell'>{item.teachers.join(',')}</td>
+            <td className='flex items-center gap-4 p-4'>{item.subject}</td> 
+            <td className=''>{item.class}</td>
+            <td className='hidden md:table-cell'>{item.teacher}</td>
+            <td className='hidden md:table-cell'>{item.date}</td>
             <td>
                 <div className='flex items-center gap-2'>
                     <Link href={`/list/teachers/${item.id}`}>
@@ -53,7 +66,7 @@ const SubjectListPage = () => {
         <div className='bg-white p-4 rounded-md flex-1 m-4 mt-0 h-[750px]'>
             {/* TOP */}
             <div className='flex items-center justify-between'>
-                <h1 className='hidden md:block text-lg font-semibold'>All Subject</h1>
+                <h1 className='hidden md:block text-lg font-semibold'>All Exams</h1>
                 <div className='flex flex-col md:flex-row items-center gap-4  w-full md:w-auto'>
                     <TableSearch />
                     <div className='flex items-center gap-4 self-end'>
@@ -71,11 +84,11 @@ const SubjectListPage = () => {
                 </div>
             </div>
             {/* LIST */}
-            <Table columns={columns} renderRow={renderRow} data={subjectsData}/>          
+            <Table columns={columns} renderRow={renderRow} data={examsData}/>          
             {/* PAGINATION */}
             <Pagination />
         </div>              
     );
 };
 
-export default SubjectListPage;
+export default ExamListPage;
